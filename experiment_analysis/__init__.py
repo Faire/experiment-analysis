449601d7a6dd86e08a09abc61837cddc1b8486c8
eramirezritchie <77996870+eramirezritchie@users.noreply.github.com>
2021-12-06 22:29:36 -0800

    Update __init__.py

--- experiment_analysis/__init__.py
+++ experiment_analysis/__init__.py
@@ -2,5 +2,5 @@ from .cuped import apply_cuped, CupedMethod
 from .capping import apply_capping, CappingMethod
 from .power_calculations import calculate_samples, calculate_mde
 from .ratio_metric import calc_ratio_metric_var, ttest_ratio_metric
-from .plotting import get_timeseries, plot_timeseries
+from .plotting import get_timeseries, plot_timeseries, TimeseriesMethod
 from .ttest import summarize_ttest
